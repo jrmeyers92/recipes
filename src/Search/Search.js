626920ffa1f619cc1e36dcadfa17f91fc78a6bd2
Jake Meyers <jakejake92@yahoo.com>
2020-11-12 19:47:56 -0700

    api for recipe working

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import "./Search.css";
 
 const Search = (props) => {
+	const API_KEY = "76bad4fc24f14d7dba30f3640dc73941";
 	const [searchTerm, setSearchTerm] = useState("");
 
 	const onFormSubmit = (e) => {
 		e.preventDefault();
+
+		props.onSearchSubmit(searchTerm);
 	};
 
 	return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import Search from "./Search/Search";
+
+const API_KEY = "76bad4fc24f14d7dba30f3640dc73941";
 
 const App = () => {
 	const onSearchSubmit = (searchTerm) => {
-		console.log(searchTerm);
+		axios.get(
+			`https://api.spoonacular.com/recipes/complexSearch?query=${searchTerm}&apiKey=${API_KEY}`
+		);
 	};
 
 	return (
